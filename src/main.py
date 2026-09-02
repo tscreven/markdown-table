7184932c37979821c19960c5d0bcced1fa4eff2e
tscreven <tscreven@gmail.com>
2026-09-02 13:21:34 -0700

    File reorganization for package publishing

--- src/main.py
+++ src/main.py
@@ -2,18 +2,21 @@ import argparse
 from table_generator import MarkdownTable
 
 def main():
-    parser = argparse.ArgumentParser("fill in")
-    parser.add_argument("f", help="Path to file.")
-    parser.add_argument("md", help="Markdown file.")
+    parser = argparse.ArgumentParser(
+        description="Generate Markdown tables from CSV, Excel, or NumPy files."
+    )
+    parser.add_argument("f", help="Path to data file.")
+    parser.add_argument("md", help="Path to target Markdown file.")
     parser.add_argument("-cols", nargs='+', 
-                        help="Column headers to display from data file.")
+                        help="Column headers to include from the data file.")
     parser.add_argument("-sheets", nargs='+', 
-                        help="Sheet names to generate table for if data file is an Excel file.")
+                        help="Excel sheet names to generate tables from.")
     parser.add_argument("-align", choices=["left", "center", "right"], 
-                        default="center", help="Markdown file.")
+                        default="center", help="Table column alignment.")
     parser.add_argument("-line", type=int, default=0,
-                        help="Line number where table(s) are written to in Markdown file.")
-    parser.add_argument("-append", action='store_true', help="Append table to end of Markdown file.")
+                        help="Line number where table(s) are inserted in the Markdown file.")
+    parser.add_argument("-append", action='store_true', 
+                        help="Append table(s) to the end of the Markdown file.")
 
     args = parser.parse_args()
 
